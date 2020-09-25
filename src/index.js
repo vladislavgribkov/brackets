becf3fc5d8a5e534f07d1d8cbbcc846a97280412
vladislavgribkov <vladislavgrbkovq@yandex.by>
2020-09-25 17:21:25 +0300

    feat: complited task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig, arr=[]) {
+    let reg;
+    let array = [];
+    
+    label: for(let i = 0; i < bracketsConfig.length; i++){
+    if(!str.match(/^-{0,1}\d+$/)){
+        reg = new RegExp(`\\${bracketsConfig[i][0]}\\${bracketsConfig[i][1]}`,'g');
+    }else{
+        reg = new RegExp(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`,'g');
+    }
+        array = str.match(reg);
+        if(array!==null){
+            str = str.replace(reg,'');
+            arr.push(str);
+            check(str, bracketsConfig,arr);
+            break label;
+        }
+    }
+    return arr[arr.length-1]==='';
 }
